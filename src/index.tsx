7e4308625215cdb0def824d1d9a6fb27b7da08a4
Alex Bolotskov <mamoruuu@proton.me>
2024-11-07 22:14:03 +0500

    refactor: replaces ReactDOM.render() call

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import './index.css'
@@ -7,14 +7,14 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './store'
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = createRoot(container)
+root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
 	</React.StrictMode>
-	,
-	document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
