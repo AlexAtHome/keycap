0fd46e4650d153f63365bd250151d7b8fdf14366
Alex Bolotskov <mamoruuu@proton.me>
2024-11-17 05:46:08 +0500

    feat: PWA

--- src/functions/is-macos.ts
+++ src/functions/is-macos.ts
@@ -1,9 +1,17 @@
+/** @see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/userAgentData */
+type ExperimentalNavigator = Navigator & {
+	userAgentData?: {
+		platform: string;
+	}
+}
+
 /**
  * @see https://codepen.io/webs95/pen/ZERPWdO
  */
 export function isMacOS(): boolean {
+	const nav = navigator as ExperimentalNavigator;
 	const platforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K", "macOS"];
-	const current = navigator?.userAgentData?.platform || navigator.platform;
+	const current = nav.userAgentData?.platform || nav.platform;
 
 	return platforms.includes(current)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(),
+	VitePWA({
+		registerType: 'autoUpdate',
+		devOptions: {
+			enabled: false
+		},
+		workbox: {
+			globPatterns: ['**/*.{js,css,html}', '**/*.{ico,png,svg}']
+		},
+		manifest: {
+			name: "Keycap",
+			short_name: "Keycap",
+			description: "Keyboard Testing TOol",
+			theme_color: "#1e293b",
+			background_color: "#1e293b",
+			start_url: "/",
+			display: "standalone",
+			categories: [
+				"productivity"
+			],
+			"icons": [
+				{
+					"src": "pwa-64x64.png",
+					"sizes": "64x64",
+					"type": "image/png"
+				},
+				{
+					"src": "pwa-192x192.png",
+					"sizes": "192x192",
+					"type": "image/png"
+				},
+				{
+					"src": "pwa-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png"
+				},
+				{
+					"src": "maskable-icon-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png",
+					"purpose": "maskable"
+				}
+			]
+		}
+	})
+	],
 })
