cfc2483e999915fce7c7667e91953d7bcdb80799
Alex Bolotskov <mamoruuu@proton.me>
2025-04-07 22:07:12 +0500

    fix: disconnected device warning icon size

--- src/routes/device.tsx
+++ src/routes/device.tsx
@@ -23,7 +23,7 @@ const GamepadPageLayout = () => {
 		<h2 className="font-bold text-4xl mb-2">{gamepad.name}</h2>
 		{!gamepad.isConnected && <div className="rounded-lg p-2 bg-yellow-600 text-black">
 			<h3 className="font-bold text-2xl mb-2 inline-flex gap-2 items-baseline">
-				<ExclamationTriangle aria-hidden={true} />
+				<ExclamationTriangle aria-hidden={true} className="w-8 self-center" />
 				Looks like the gamepad has been disconnected!
 			</h3>
 			<p>Make sure it is properly connected to the computer via cable or Bluetooth, then try to press any button.</p>
